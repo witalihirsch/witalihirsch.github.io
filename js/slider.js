15c9604759d042c164ae226c66ad7481ce748f5b
witalihirsch <witalihirsch1@gmail.com>
2023-05-31 14:05:28 +0600

    portfolio

--- js/slider.js
+++
@@ -1,6 +0,0 @@
-for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
-    e.style.setProperty('--value', e.value);
-    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
-    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
-    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
-}
