d62a1a293de9d4946df7a9e247daefffa887b229
witalihirsch <witalihirsch1@gmail.com>
2023-07-01 17:22:31 +0600

    drive! app

--- js/popup.js
+++ js/popup.js
@@ -5,11 +5,13 @@ let link1 = document.querySelector("#item__link-1");
 let link2 = document.querySelector("#item__link-2");
 let link3 = document.querySelector("#item__link-3");
 let link4 = document.querySelector("#item__link-4");
+let link5 = document.querySelector("#item__link-5");
 
 let popup1 = document.querySelector("#popup-1");
 let popup2 = document.querySelector("#popup-2");
 let popup3 = document.querySelector("#popup-3");
 let popup4 = document.querySelector("#popup-4");
+let popup5 = document.querySelector("#popup-5");
 
 link1.addEventListener("click", function() {
     popup.style.display = "flex";
@@ -31,10 +33,16 @@ link4.addEventListener("click", function() {
     popup4.style.display = "flex";
 })
 
+link5.addEventListener("click", function() {
+    popup.style.display = "flex";
+    popup5.style.display = "flex";
+})
+
 headerIcon.addEventListener("click", function() {
     popup.style.display = "none";
     popup1.style.display = "none";
     popup2.style.display = "none";
     popup3.style.display = "none";
     popup4.style.display = "none";
+    popup5.style.display = "none";
 })
