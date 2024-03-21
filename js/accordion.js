5a07cc81ca7f3a5fb3c0ab903550d2c8ef7d6c61
witalihirsch <witalihirsch1@gmail.com>
2024-03-21 13:05:10 +0500

    email form

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,14 @@
-const accordionItems = document.querySelectorAll('.accordion__item');
+var acc = document.getElementsByClassName("accordion__item");
+var i;
 
-accordionItems.forEach(item => {
-    const title = item.querySelector('.accordion__title');
-
-    title.addEventListener('click', () => {
-        item.classList.toggle('active');
-    });
-});
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.querySelector('.panel');
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
